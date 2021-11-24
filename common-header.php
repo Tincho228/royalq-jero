1b27c337d1024f51887a173c44e2d5dff40192a4
Tincho228 <martin_quintero521@hotmail.com>
2021-11-24 16:27:36 -0300

    second block

--- common-header.php
+++ common-header.php
@@ -15,20 +15,19 @@
             <a class="nav-link text-light" aria-current="page" href="./index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="./index.php#howToStart"><i class="far fa-question-circle"></i> Como empiezo</a>
+            <a class="nav-link text-light" href="./index.php#howToStart">Como empiezo</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="index.php?action=tutorials"><i class="fas fa-book"></i> Tutoriales</a>
+            <a class="nav-link text-light" href="index.php?action=tutorials">Tutoriales</a>
           </li>
           <li class="nav-item">
             <a class="nav-link text-light"
               href="<?php if(isset($user_facebook_link)){echo $user_facebook_link;} ?>"
-              target="_blank"><i class="fab fa-facebook"></i> Seguinos</a>
+              target="_blank">Seguinos en facebook</a>
           </li>
           <li class="nav-item">
             <!-- Button trigger modal -->
-            <a class="nav-link text-light" data-toggle="modal" data-target="#contact-modal"><i
-                class="fas fa-envelope-open-text"></i> Contacto</a>
+            <a class="nav-link text-light" data-toggle="modal" data-target="#contact-modal">Contacto</a>
           </li>
         </ul>
       </div>
--- view-home.php
+++ view-home.php
@@ -37,11 +37,13 @@
                         <h1 class="display-3 animate__animated animate__fadeInUp" style="line-height: 0.9;">Start</h1>
                         <h1 class="display-3 animate__animated animate__fadeInUp" style="line-height: 0.9;">trading</h1>
                         <h1 class="display-3 animate__animated animate__fadeInUp"
-                            style="line-height: 0.9; margin-bottom:40px;">Binance</h1>
-                        <a class="btn btn-info bg-main-color" style="padding:8px 30px; margin-bottom:10px" href="">Registrate</a>
-                        <p class="text-light"> Sistemas automatico<br> de Ganancias</p>
+                            style="line-height: 0.9; margin-bottom:10px;">Binance</h1>
+                        <p class="text-light" style="margin-bottom:30px"> Sistemas automatico<br> de Ganancias</p>
+                        <a class="btn btn-info bg-main-color" style="padding:8px 30px; margin-bottom:70px"
+                            href="">Registrate</a>
+
                     </div>
-                    <div class="col-sm-12 col-md-6" >
+                    <div class="col-sm-12 col-md-6">
                         <div class="d-flex justify-content-center" style="margin-bottom: 30px;">
                             <img src="./images/bitcoin.png" alt="image of bitcoin"
                                 style="width:70px; margin-right:10px">
@@ -56,6 +58,43 @@
             </div>
         </div>
 
+    </div>
+    <!-- What is royalQ -->
+    <div class="container-fluid bg-first-color">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-lg-8">
+                <div class="row" style="margin:40px 0px">
+                    <div class="col-sm-12 col-md-6">
+                        <h4 class="bg-second-color">Que es Royal Q</h2>
+                            <h1 class="text-light">Robot automatico</h1>
+                            <h4 class="text-light"style="margin-bottom:30px">Secundario</h4>
+                            <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+                                consequuntur distinctio dignissimos similique velit atque eius fugit, magni, sed et sunt
+                                dolores vitae minus laboriosam necessitatibus facilis quam obcaecati. Iusto!</p>
+                            <div class="d-flex">
+                                <img src="./images/tilde.png" alt="imagen de check" style="width:30px; height:30px; margin-right:5px;">
+                                <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.</p>
+                            </div>
+                            <div class="d-flex">
+                                <img src="./images/tilde.png" alt="imagen de check" style="width:30px; height:30px; margin-right:5px;">
+                                <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.</p>
+                            </div>
+                            <div class="d-flex">
+                                <img src="./images/tilde.png" alt="imagen de check" style="width:30px; height:30px; margin-right:5px;">
+                                <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.</p>
+                            </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <img class="img-fluid" style="margin-top:50px" src="./images/app-group.png" alt="imagen de la app">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color" style="line-height: 1.5; margin-right:10px; margin-bottom:20px;">Registrate en RoyalQ</a></div>
+                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color" style="line-height: 1.5;margin-right:10px; margin-bottom:20px">Registrate en Binance</a></div>
+                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color" style="line-height: 1.5;">Contacta a un asesor</a></div>
+                </div>
+            </div>
+        </div>
     </div>
     <!--  Footer  -->
     <?php include "./common-footer.php" ?>
