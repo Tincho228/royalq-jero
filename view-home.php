aeaad70175caf392238be833827fe6342eeab49c
Tincho228 <martin_quintero521@hotmail.com>
2021-11-25 16:08:17 -0300

    adding-video

--- view-home.php
+++ view-home.php
@@ -167,7 +167,8 @@
                         <div>
                             <h5 class="text-light text-center">Royal Q<br>no retiene tu inversion</h5>
                             <p class="text-center text-light">RoyalQ no retiene su capital, pero se financia mediante
-                                una suscripción anual de $ 100. Sus fondos permanecen en su intercambio (Binance o Huobi)
+                                una suscripción anual de $ 100. Sus fondos permanecen en su intercambio (Binance o
+                                Huobi)
                                 que está conectado a través de API.
                                 RoyalQ no tiene acceso a sus fondos, que permanecen seguros en el intercambio.</p>
                         </div>
@@ -668,6 +669,44 @@
             </div>
         </div>
     </div>
+
+
+    <div class="modal fade" id="intro-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content bg-last-color">
+                <div class="modal-header align-items-center">
+                    <img class="mail_icon" src="./images/logo.png" alt="email icon" style="width:30px;">
+                    <h5 class="modal-title bg-second-color" style="margin-left:10px;" id="exampleModalLabel">Video intro
+                    </h5>
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="text-light">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-light">
+                    <div class="embed-responsive embed-responsive-16by9"
+                        style="margin-bottom:20px; box-shadow: 10px 9px 20px #0000008c;">
+                        <video id="video-intro" controls autoplay muted width="500">
+                            <source id="mp4_src" src="./videos/intro.mp4" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <script>
+        var modal = document.getElementById("intro-modal");
+        var video = document.getElementById("video-intro");
+        modal.style.display = "block";
+        modal.classList.add("show");
+        video.play();
+    </script>
+
+
+
     <!--  Floating-menu  -->
     <?php include "./view-floating-menu.php" ?>
     <!--  Footer  -->
@@ -681,6 +720,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
+
+
 </body>
 
 </html>
---
+++ view-modal-intro.php
@@ -0,0 +1,27 @@
+<!-- MODAL Video Intro -->
+
+<div class="modal fade" id="intro-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content bg-last-color">
+            <div class="modal-header align-items-center">
+                <img class="mail_icon" src="./images/logo.png" alt="email icon" style="width:30px;">
+                <h5 class="modal-title bg-second-color" style="margin-left:10px;" id="exampleModalLabel">Video intro
+                </h5>
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" class="text-light">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-light">
+                <div class="embed-responsive embed-responsive-16by9"
+                    style="margin-bottom:20px; box-shadow: 10px 9px 20px #0000008c;">
+                    <video id="video-intro" controls width="500">
+                        <source id="mp4_src" src="./videos/intro.mp4" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
