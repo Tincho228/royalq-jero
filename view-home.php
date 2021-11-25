e3c517509f5dc0f6da9411b2005ef782a894957b
Tincho228 <martin_quintero521@hotmail.com>
2021-11-24 23:34:10 -0300

    adding third section

--- view-home.php
+++ view-home.php
@@ -61,37 +61,149 @@
     </div>
     <!-- What is royalQ -->
     <div class="container-fluid bg-first-color">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center" style="padding:40px 0px">
             <div class="col-sm-12 col-lg-8">
-                <div class="row" style="margin:40px 0px">
+                <div class="row">
                     <div class="col-sm-12 col-md-6">
-                        <h4 class="bg-second-color">Que es Royal Q</h2>
-                            <h1 class="text-light">Robot automatico</h1>
-                            <h4 class="text-light"style="margin-bottom:30px">Secundario</h4>
-                            <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-                                consequuntur distinctio dignissimos similique velit atque eius fugit, magni, sed et sunt
-                                dolores vitae minus laboriosam necessitatibus facilis quam obcaecati. Iusto!</p>
-                            <div class="d-flex">
-                                <img src="./images/tilde.png" alt="imagen de check" style="width:30px; height:30px; margin-right:5px;">
-                                <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.</p>
-                            </div>
-                            <div class="d-flex">
-                                <img src="./images/tilde.png" alt="imagen de check" style="width:30px; height:30px; margin-right:5px;">
-                                <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.</p>
-                            </div>
-                            <div class="d-flex">
-                                <img src="./images/tilde.png" alt="imagen de check" style="width:30px; height:30px; margin-right:5px;">
-                                <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.</p>
-                            </div>
+                        <h4 class="bg-second-color">Que es Royal Q</h4>
+                        <h1 class="text-light">Robot automatico</h1>
+                        <h4 class="text-light" style="margin-bottom:30px">Secundario</h4>
+                        <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+                            consequuntur distinctio dignissimos similique velit atque eius fugit, magni, sed et sunt
+                            dolores vitae minus laboriosam necessitatibus facilis quam obcaecati. Iusto!</p>
+                        <div class="d-flex">
+                            <img src="./images/tilde.png" alt="imagen de check"
+                                style="width:30px; height:30px; margin-right:5px;">
+                            <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.
+                            </p>
+                        </div>
+                        <div class="d-flex">
+                            <img src="./images/tilde.png" alt="imagen de check"
+                                style="width:30px; height:30px; margin-right:5px;">
+                            <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.
+                            </p>
+                        </div>
+                        <div class="d-flex">
+                            <img src="./images/tilde.png" alt="imagen de check"
+                                style="width:30px; height:30px; margin-right:5px;">
+                            <p class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste.
+                            </p>
+                        </div>
                     </div>
                     <div class="col-sm-12 col-md-6">
-                        <img class="img-fluid" style="margin-top:50px" src="./images/app-group.png" alt="imagen de la app">
+                        <img class="img-fluid" style="margin-top:50px" src="./images/app-group.png"
+                            alt="imagen de la app">
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color" style="line-height: 1.5; margin-right:10px; margin-bottom:20px;">Registrate en RoyalQ</a></div>
-                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color" style="line-height: 1.5;margin-right:10px; margin-bottom:20px">Registrate en Binance</a></div>
-                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color" style="line-height: 1.5;">Contacta a un asesor</a></div>
+                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color"
+                            style="line-height: 1.5; margin-right:10px; margin-bottom:20px;">Registrate en RoyalQ</a>
+                    </div>
+                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color"
+                            style="line-height: 1.5;margin-right:10px; margin-bottom:20px">Registrate en Binance</a>
+                    </div>
+                    <div class="col-sm-12 col-md-3"><a href="" class="btn btn-info btn-block bg-main-color"
+                            style="line-height: 1.5;">Contacta un asesor</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Characteristics -->
+    <div class="container-fluid bg-middle-color">
+        <div class="row justify-content-center" style="padding:40px 0px">
+            <div class="col-sm-12 col-lg-8">
+                <h4 class="bg-second-color text-center">Características</h4>
+                <h1 class="text-light text-center">¿No sabes nada de trading pero quieres invertir?</h1>
+                <h4 class="text-light text-center" style="margin-bottom:30px">Tres razones para elegir Royal Q.</h4>
+                <div class="row" style="margin:70px 0px">
+                    <div class="col-sm-12 col-md-4">
+                        <div class="d-flex justify-content-center">
+                            <img src="./images/time.png" alt="Imagen Icono de tiempo" class="img-fluid"
+                                style="width:70px">
+                        </div>
+                        <div style="margin-bottom:50px;">
+                            <h5 class="text-light text-center">Ahorra tiempo<br>y recursos</h5>
+                            <p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Sapiente, ab. Expedita earum placeat dolor numquam.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4">
+                        <div class="d-flex justify-content-center">
+                            <img src="./images/inversion.png" alt="Imagen Icono de inversion" class="img-fluid"
+                                style="width:70px">
+                        </div>
+                        <div style="margin-bottom:50px;">
+                            <h5 class="text-light text-center">Inversión<br>sin emociones</h5>
+                            <p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Sapiente, ab. Expedita earum placeat dolor numquam.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4">
+                        <div class="d-flex justify-content-center">
+                            <img src="./images/ganancia.png" alt="Imagen Icono de ganancia" class="img-fluid"
+                                style="width:70px">
+                        </div>
+                        <div>
+                            <h5 class="text-light text-center">Ganancias<br>en tiempo real</h5>
+                            <p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Sapiente, ab. Expedita earum placeat dolor numquam.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- How to start -->
+    <div class="container-fluid bg-third-color">
+        <div class="row justify-content-center" style="padding:40px 0px">
+            <div class="col-sm-12 col-lg-8">
+                <h4 class="bg-second-color text-center">Guía rápida</h4>
+                <h1 class="text-light text-center">¿Cómo empiezo?</h1>
+                <h4 class="text-light text-center" style="margin-bottom:30px">Sólo son tres pasos.</h4>
+                <p class="text-center text-light">Recuerda que tienes a disposición todos los tutoriales que
+                                te guiaran paso a paso.
+                                También puedes enviarnos un mensaje y un asesor se pondrá
+                                en contacto a la brevedad.
+                            </p>
+                <div class="row" style="margin:70px 0px">
+                    <div class="col-sm-12 col-md-4">
+                        <div class="d-flex justify-content-center">
+                            <img src="./images/royal.png" alt="Imagen Icono de Royal con reflexion" class="img-fluid"
+                                style="width:70px">
+                        </div>
+                        <div style="margin-bottom:50px;">
+                            <h5 class="text-light text-center">Creá una<br>cuenta en Royal Q</h5>
+                            <p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Sapiente, ab. Expedita earum placeat dolor numquam.
+                            </p>
+                            <a class="btn btn-info bg-main-color btn-block" href="">PASO 1 - Registrate</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4">
+                        <!-- <h5 class="text-light text-center">PASO 1</h5> -->
+                        <div class="d-flex justify-content-center">
+                            <img src="./images/binance.png" alt="Imagen Icono de binance" class="img-fluid"
+                                style="width:135px">
+                        </div>
+                        <div style="margin-bottom:50px;">
+                            <h5 class="text-light text-center">Crea una<br>cuenta en Binance</h5>
+                            <p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Sapiente, ab. Expedita earum placeat dolor numquam.</p>
+                            <a class="btn btn-info bg-main-color btn-block" href="">PASO 2 - Registrate</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4">
+                        <div class="d-flex justify-content-center">
+                            <img src="./images/vinculo.png" alt="Imagen Icono de vinculo" class="img-fluid"
+                                style="width:70px">
+                        </div>
+                        <div>
+                            <h5 class="text-light text-center">Vinculá<br>tus cuentas</h5>
+                            <p class="text-center text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Sapiente, ab. Expedita earum placeat dolor numquam.</p>
+                            <a class="btn btn-info bg-main-color btn-block" href="">PASO 3 - Mirá el tutorial</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
