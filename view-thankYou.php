f0d60f367c8a388e4993b83aa67245407b471f3f
Tincho228 <martin_quintero521@hotmail.com>
2021-11-25 14:32:24 -0300

    thankyou page

---
+++ view-thankYou.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="./css/styles.css">
+
+    <!-- Favicon Code -->
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+</head>
+
+<body>
+    <div class="container-fluid bg-last-color">
+        <!-- Nav bar -->
+        <?php include "./common-header.php" ?>
+        <!-- Main title -->
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-lg-8">
+                <div class="row" style="margin:13vh 0px">
+                    <div class="col-sm-12 col-md-6 text-light">
+                        <h5 class="bg-second-color">Respuesta</h5>
+                        <h1 class="text-light display-4">Gracias por tu mensaje</h1>
+                        <p>Por otras consultas, no olvides visitar<br>nuestros tutoriales</p>
+                        <a class="btn btn-outline-secondary text-light" href="./index.php#tutoriales" style="margin-bottom:30px">Ir a tutoriales</a>
+
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <img src="./images/app-cellphone.png" style="width:160px; margin-bottom:30px" alt="Imagen de celular">
+                        <div class="d-flex">
+                            <img src="./images/tilde.png" alt="imagen de check"
+                                style="width:30px; height:30px; margin-right:5px;">
+                            <p class="text-light">Trabaja las 24 horas.
+                            </p>
+                        </div>
+                        <div class="d-flex">
+                            <img src="./images/tilde.png" alt="imagen de check"
+                                style="width:30px; height:30px; margin-right:5px;">
+                            <p class="text-light">Sin conocimientos previos de trading.
+                            </p>
+                        </div>
+                        <div class="d-flex" style="margin-bottom:30px">
+                            <img src="./images/tilde.png" alt="imagen de check"
+                                style="width:30px; height:30px; margin-right:5px;">
+                            <p class="text-light">Desde la comodidad del hogar.
+                            </p>
+                        </div>
+                        <a class="btn btn-outline-secondary text-light" href="./index.php">Volver al inicio</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+   
+    
+    <!--  Floating-menu  -->
+    <?php include "./view-floating-menu.php" ?>
+    <!--  Footer  -->
+    <?php include "./common-footer.php" ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
